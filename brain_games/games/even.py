13b044e87ff435c5246545787a404095df3721b5
MaukhetdinovEF <tore1sk@gmail.com>
2023-06-13 03:58:18 +0500

    fixed game even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,29 +1,17 @@
-#!/usr/bin/env python3
-
-
 import prompt
 from random import randint
 
 
-def main():
+def game_even():
     print('Welcome to the Brain Games!')
-    name = welcome_user()
-    return name
-
-
-def welcome_user():
     name = prompt.string('May I have your name ? ')
     print(f'Hello, {name}!')
-    return name
-
-
-def even(name):
-    print('Answe "yes" if the number is even, otherwise answer "no".')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     item = 0
     while item < 3:
         number = randint(1, 9999999)
-        print(f'Question: {number}')
-        join = prompt.string('Your answer: ')
+        print(f"Question: {number}")
+        join = prompt.string("Your answer: ")
         if number % 2 == 0:
             unswer = 'yes'
         else: unswer = 'no'  
@@ -32,11 +20,6 @@ def even(name):
             print('Correct!')
         else:
             item = 4
-            print(f"'{join}' is wrong answer ;(. Correct answer was {unswer}\nLet's try again, {name}!")
+            print(f"'{join}' is wrong answer ;(. Correct answer was '{unswer}'\nLet's try again, {name}!")
         if item == 3:
             print(f'Congratulation, {name}!')
-
-
-if __name__ == '__main__':
-    names = main()
-    even(names)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,42 +1,12 @@
 #!/usr/bin/env python3
 
 
-import prompt
-from random import randint
+from brain_games.games.even import game_even
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = welcome_user()
-    return name
-
-
-def welcome_user():
-    name = prompt.string('May I have your name ? ')
-    print(f'Hello, {name}!')
-    return name
-
-
-def even(name):
-    print('Answe "yes" if the number is even, otherwise answer "no".')
-    item = 0
-    while item < 3:
-        number = randint(1, 9999999)
-        print(f"Question: {number}")
-        join = prompt.string("Your answer: ")
-        if number % 2 == 0:
-            unswer = 'yes'
-        else: unswer = 'no'  
-        if unswer == join.lower():
-            item += 1
-            print('Correct!')
-        else:
-            item = 4
-            print(f"'{join}' is wrong answer ;(. Correct answer was '{unswer}'\nLet's try again, {name}!")
-        if item == 3:
-            print(f'Congratulation, {name}!')
+    game_even()
 
 
 if __name__ == '__main__':
-    names = main()
-    even(names)
+    main()
